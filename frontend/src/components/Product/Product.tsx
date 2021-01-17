12ce1a1e75165f30a154b70ac04300dec6b4d841
Julian <julesnuggy@gmail.com>
2021-01-17 18:03:18 +0000

    Create Products component

---
+++ frontend/src/components/Product/Product.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+
+import Icon from "../Icon/Icon";
+
+import styles from './Product.module.scss';
+
+type props = {
+  title: string;
+  imageName: string;
+  description: string;
+}
+
+const Product = ({ title, imageName, description }: props) => (
+  <div className={styles.container}>
+    <div className={styles.title}>
+      {title}
+    </div>
+    <div className={styles.image}>
+      <Icon name={imageName}/>
+    </div>
+    <div className={styles.description}>
+      {description}
+    </div>
+  </div>
+);
+
+export default Product;
--- frontend/src/pages/Items.tsx
+++ frontend/src/pages/Items.tsx
@@ -1,12 +1,15 @@
 import React from 'react';
 
 import Page from "../components/Page/Page";
+import Product from "../components/Product/Product";
 
 const Items = () => (
   <Page title="Items">
-    <p>
-      Items
-    </p>
+    <Product title="Potion" imageName="heartPotion" description="Heals 50 health" />
+    <Product title="Potion" imageName="heartPotion" description="Heals 50 health" />
+    <Product title="Potion" imageName="heartPotion" description="Heals 50 health" />
+    <Product title="Potion" imageName="heartPotion" description="Heals 50 health" />
+    <Product title="Potion" imageName="heartPotion" description="Heals 50 health" />
   </Page>
 );
 
