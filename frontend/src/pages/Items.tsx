08d6c1d7645002ebf1865aa79ac2588fba13de80
Julian <julesnuggy@gmail.com>
2021-01-17 18:07:18 +0000

    Add styles to Items page

--- frontend/src/pages/Items.tsx
+++ frontend/src/pages/Items.tsx
@@ -3,13 +3,17 @@ import React from 'react';
 import Page from "../components/Page/Page";
 import Product from "../components/Product/Product";
 
+import styles from './Items.module.scss';
+
 const Items = () => (
   <Page title="Items">
-    <Product title="Potion" imageName="heartPotion" description="Heals 50 health" />
-    <Product title="Potion" imageName="heartPotion" description="Heals 50 health" />
-    <Product title="Potion" imageName="heartPotion" description="Heals 50 health" />
-    <Product title="Potion" imageName="heartPotion" description="Heals 50 health" />
-    <Product title="Potion" imageName="heartPotion" description="Heals 50 health" />
+    <div className={styles.productsContainer}>
+      <Product title="Potion" imageName="heartPotion" description="Heals 50 health" />
+      <Product title="Potion" imageName="heartPotion" description="Heals 50 health" />
+      <Product title="Potion" imageName="heartPotion" description="Heals 50 health" />
+      <Product title="Potion" imageName="heartPotion" description="Heals 50 health" />
+      <Product title="Potion" imageName="heartPotion" description="Heals 50 health" />
+    </div>
   </Page>
 );
 
