7e391a54a1db70a6f079554bf472e6d326470f32
Julian <julesnuggy@gmail.com>
2021-01-18 14:03:01 +0000

    Create Product POST endpoint

---
+++ server/src/controllers/productsController.ts
@@ -0,0 +1,20 @@
+import {
+  Body,
+  Controller,
+  Post,
+  Route,
+  SuccessResponse,
+} from 'tsoa';
+import { Product } from '../models/product';
+import { ProductsService, CreateProductParams } from "../services/productsService";
+
+@Route('products')
+export class ProductsController extends Controller {
+
+  @SuccessResponse('200', 'Created')
+  @Post()
+  public async createProduct(@Body() product: CreateProductParams): Promise<Product> {
+    this.setStatus(200);
+    return new ProductsService().createProduct(product);
+  }
+}
---
+++ server/src/services/productsService.ts
@@ -0,0 +1,13 @@
+import Product from '../models/product';
+import { v4 as uuidv4 } from 'uuid';
+
+export type CreateProductParams = Pick<Product, 'title' | 'description' | 'imageName' | 'stock'>;
+
+export class ProductsService {
+  public createProduct = (params: CreateProductParams): Product => {
+    return {
+      id: uuidv4(),
+      ...params
+    }
+  }
+}
