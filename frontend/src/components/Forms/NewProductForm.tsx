0c9d561d243a6a66eaa6dac52f6903d93c9490c2
Julian <julesnuggy@gmail.com>
2021-03-05 15:40:11 +0000

    Refactor Archmagisatraion form

---
+++ frontend/src/components/Forms/NewProductForm.tsx
@@ -0,0 +1,48 @@
+import React from "react";
+import { Field, Form, Formik } from "formik";
+import { Product } from "../../models/Product";
+
+import styles from './forms-common.module.scss';
+
+type FormProps = {
+  onSubmit: (values: Product, { resetForm }) => void;
+}
+
+export const NewProductForm = ({ onSubmit }: FormProps) => {
+  const titleRef = React.createRef();
+  const initialValues: Product = ({ title: '', description: '', imageName: '', stock: 0 });
+
+  const handleSubmit = (values: Product, { resetForm }) => {
+    onSubmit(values, resetForm);
+    titleRef.current?.focus();
+  };
+
+  return (
+    <Formik initialValues={initialValues} onSubmit={handleSubmit}>
+      {() => (
+        <Form className={styles.form} title="Add New Product" name="newProduct">
+          <b>Add New Product</b>
+          <div className={styles.formItem}>
+            <label htmlFor="title">Title</label>
+            <Field name="title" title="title" innerRef={titleRef} />
+          </div>
+          <div className={styles.formItem}>
+            <label htmlFor="description">Description</label>
+            <Field name="description" title="Description" />
+          </div>
+          <div className={styles.formItem}>
+            <label htmlFor="imageName">Image Name</label>
+            <Field name="imageName" title="Image Name" />
+          </div>
+          <div className={styles.formItem}>
+            <label htmlFor="stock">Stock</label>
+            <Field name="stock" title="Stock" />
+          </div>
+          <div className={styles.formSubmit}>
+            <button type="submit">Submit New Product</button>
+          </div>
+        </Form>
+      )}
+    </Formik>
+  );
+}
--- frontend/src/pages/Archmagistration.tsx
+++ frontend/src/pages/Archmagistration.tsx
@@ -1,12 +1,10 @@
 import React, { useCallback } from "react";
-import { Formik, Form, Field } from 'formik';
 
 import Page from '../components/Page/Page';
 import { Product } from "../models/Product";
 import { createProduct } from "../services/servatorium";
 import { useRequestState } from "../utils/hooksUtils";
-
-import styles from './Archmagistration.module.scss'
+import { NewProductForm } from "../components/Forms/NewProductForm";
 
 const useProductsRequests = () => {
   const callCreateProduct = useCallback((product: Product) =>  createProduct(product), []);
@@ -22,43 +20,15 @@ const useProductsRequests = () => {
 
 const Archmagistration = () => {
   const { callCreateProduct } = useProductsRequests();
-  const titleRef = React.createRef();
-  const initialValues: Product = ({ title: '', description: '', imageName: '', stock: 0 })
 
-  const handleSubmit = async (values: Product, { resetForm }) => {
+  const onSubmitProduct = async (values: Product, { resetForm }) => {
     await callCreateProduct(values);
     resetForm();
-    titleRef.current?.focus();
   }
 
   return (
     <Page title="Archmagistration">
-      <Formik initialValues={initialValues} onSubmit={handleSubmit}>
-        {() => (
-          <Form className={styles.productForm} title="Add New Product" name="newProduct">
-            <b>Add New Product</b>
-            <div className={styles.formItem}>
-              <label htmlFor="title">Title</label>
-              <Field name="title" title="title" innerRef={titleRef} />
-            </div>
-            <div className={styles.formItem}>
-              <label htmlFor="description">Description</label>
-              <Field name="description" title="Description" />
-            </div>
-            <div className={styles.formItem}>
-              <label htmlFor="imageName">Image Name</label>
-              <Field name="imageName" title="Image Name" />
-            </div>
-            <div className={styles.formItem}>
-              <label htmlFor="stock">Stock</label>
-              <Field name="stock" title="Stock" />
-            </div>
-            <div className={styles.formSubmit}>
-              <button type="submit">Submit New Product</button>
-            </div>
-          </Form>
-        )}
-      </Formik>
+      <NewProductForm onSubmit={onSubmitProduct} />
     </Page>
   )
 }
