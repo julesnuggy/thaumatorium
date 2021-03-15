de87ac1a0ab7f0e74779318ec2cc5f196d4760ce
Julian <julesnuggy@gmail.com>
2021-03-15 22:23:04 +0000

    Create context and state for logged-in user

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,38 +1,59 @@
-import React from "react";
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import React, { createContext, useState } from 'react';
+import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom';
 
-import Home from "./pages/Home";
-import Items from "./pages/Items";
-import Equipment from "./pages/Equipment";
-import Magic from "./pages/Magic";
-import Archmagistration from "./pages/Archmagistration";
-import Login from "./pages/Login";
+import Home from './pages/Home';
+import Items from './pages/Items';
+import Equipment from './pages/Equipment';
+import Magic from './pages/Magic';
+import Archmagistration from './pages/Archmagistration';
+import Login from './pages/Login';
 
+import styles from './App.module.scss';
 import './Global.module.scss';
 
-const App = (): React.FC => (
-  <BrowserRouter>
-    <Switch>
-      <Route exact path="/">
-        <Home />
-      </Route>
-      <Route path="/items">
-        <Items />
-      </Route>
-      <Route path="/equipment">
-        <Equipment />
-      </Route>
-      <Route path="/magic">
-        <Magic />
-      </Route>
-      <Route path="/archmagistration">
-        <Archmagistration />
-      </Route>
-      <Route path="/login">
-        <Login />
-      </Route>
-    </Switch>
-  </BrowserRouter>
-);
+type LoginContextType = {
+  loggedInUser: boolean,
+  setLoggedInUser: React.Dispatch<any>
+}
+
+const LoginContext = createContext<LoginContextType>({
+  loggedInUser: null,
+  setLoggedInUser: () => {}
+});
+
+const App = (): React.FC => {
+  const [loggedInUser, setLoggedInUser] = useState(null);
+  const loginContextValues = { loggedInUser, setLoggedInUser };
+
+  return (
+    <BrowserRouter>
+      <LoginContext.Provider value={loginContextValues}>
+        <div className={styles.headerBar}>
+          {loggedInUser ? `Logged in as: ${loggedInUser}` : 'Not logged in'}
+        </div>
+        <Switch>
+          <Route exact path="/">
+            <Home/>
+          </Route>
+          <Route path="/items">
+            <Items/>
+          </Route>
+          <Route path="/equipment">
+            <Equipment/>
+          </Route>
+          <Route path="/magic">
+            <Magic/>
+          </Route>
+          <Route path="/archmagistration">
+            {loggedInUser ? <Archmagistration/> : <Redirect to="/login"/>}
+          </Route>
+          <Route path="/login">
+            <Login LoginContext={LoginContext}/>
+          </Route>
+        </Switch>
+      </LoginContext.Provider>
+    </BrowserRouter>
+  );
+}
 
 export default App;
--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -1,16 +1,25 @@
-import React, { useState } from 'react';
+import React, { useState, useContext } from 'react';
+import { useHistory } from 'react-router-dom';
 
 import { LoginForm } from '../components/Forms/LoginForm';
 import Page from '../components/Page/Page';
 import { userApis } from '../services/servatorium';
 import { UserLoginFormValues } from '../models/User';
 
-const Login = () => {
+type LoginProps = {
+  LoginContext: React.Context
+}
+
+const Login = ({ LoginContext }: LoginProps) => {
+  const history = useHistory();
   const [isLoginValid, setIsLoginValid] = useState<boolean>();
+  const { setLoggedInUser } = useContext(LoginContext)
 
   const onSubmit = async (values: UserLoginFormValues) => {
     const authRes = await userApis.authenticateUser(values);
-    setIsLoginValid(authRes);
+    await setIsLoginValid(authRes);
+    setLoggedInUser(values.username);
+    history.push('/archmagistration');
   }
 
   return (
