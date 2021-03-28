89e25dd53330a8f7312f5a5bde014166daa47f9c
Julian <julesnuggy@gmail.com>
2021-03-28 10:11:47 +0100

    Correct Item POST endpoint return type

--- frontend/src/services/itemService.ts
+++ frontend/src/services/itemService.ts
@@ -5,5 +5,5 @@ export const itemsUrl = '/items';
 
 export const getItems = async (): Promise<ItemResponse[]> => client.get(itemsUrl).then(res => res.data);
 
-export const createItem = async (item: Item): Promise<Item> =>
+export const createItem = async (item: Item): Promise<void> =>
   client.post(itemsUrl, item).then(res => res.data);
