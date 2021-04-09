f65dd8203404a58fe0bea82febc8f18ab4e0bc6e
Julian <julesnuggy@gmail.com>
2021-04-09 10:20:14 +0100

    Display Magic in page

--- frontend/src/pages/Magic.tsx
+++ frontend/src/pages/Magic.tsx
@@ -1,13 +1,51 @@
-import React from 'react';
+import React, { useCallback, useEffect } from 'react';
 
 import Page from '../components/Page/Page';
+import ProductCard from '../components/ProductCard/ProductCard';
+import { magicApis } from '../services/servatorium';
+import { useRequestState } from '../utils/hooksUtils';
 
-const Magic = (): React.FC => (
-  <Page title="Magic">
-    <p>
-      Magic
-    </p>
-  </Page>
-);
+import styles from './common.module.scss';
+
+const useMagicRequests = () => {
+  const getMagicData = useCallback(() => magicApis.getMagic(), []);
+  const {
+    loading,
+    data,
+    error,
+    call,
+  } = useRequestState(getMagicData);
+
+  useEffect(() => {
+    call();
+  }, [call]);
+
+  return {
+    loading,
+    data,
+    error,
+  }
+}
+
+const Magic = (): React.FC => {
+  const { data } = useMagicRequests();
+
+  return (
+    <Page title="Magic">
+      <div className={styles.container}>
+        {data?.map((magic, idx) => (
+          <ProductCard
+            key={`${magic.title}_${idx}`}
+            title={magic.title}
+            imageName={magic.imageName}
+            description={magic.description}
+            type={magic.magicType}
+            stock={magic.stock}
+          />
+        ))}
+      </div>
+    </Page>
+  );
+}
 
 export default Magic;
